bf2d03f9e8f4c9eff1bccc7728664ffa4960afc8
park, kwangjoon <kwdark1412@gmail.com>
2025-09-17 15:39:37 +0900

    five

--- world.java
+++ world.java
@@ -2,5 +2,9 @@ public class world {
     public static void run() {
         System.out.println("202214014");
     }
+
+    public static void run2 {
+        System.out.println("hello world");
+    }
  
 }
