aecd0ef504c3fb2ee28184e04c6104a04be11972
park, kwangjoon <kwdark1412@gmail.com>
2025-09-17 15:52:22 +0900

    six

--- src/d202214014/Main.java
+++ src/d202214014/Main.java
@@ -3,8 +3,9 @@ package d202214014;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("박광준");
-		Wolrd.run();
-		Hello.run();
+		System.out.println("안녕하세요");
+		System.out.println("hello world");
+	
 	}
 
 }
